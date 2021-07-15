49c6f196e8e9f1a629b953f43c8bd64064d0a736
CH_Hwang <hch950627@naver.com>
2021-07-15 22:16:56 +0900

    validation of input
    = == ===

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const name = document.getElementById("name_btn");
 const namesize = document.getElementById("inputname")
 
 function ClickName(){
-    if(namesize.value.length = 0){
+    if(namesize.value === ""){
         alert('종목을 입력하세요.');
     }else{
         alert('종목이 입력되었습니다.');
