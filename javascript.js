fe0e2e3783d6fc4f902b1f52cbc037b2ff5db101
CH_Hwang <hch950627@naver.com>
2021-07-17 15:53:37 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const name = document.getElementById("name_btn");
 const namesize = document.getElementById("inputname")
+const price = document.getElementById("price_btn");
+const number = document.getElementById("number_btn");
 
 function ClickName(){
     if(namesize.value === ""){
@@ -9,6 +11,30 @@ function ClickName(){
     }
 }
 
+function ClickPrice(){
+    if(namesize.value === ""){
+        alert('평단가를 입력하세요.');
+    }else{
+        alert('평단가가 입력되었습니다.');
+    }
+}
+
+function ClickNumber(){
+    if(namesize.value === ""){
+        alert('갯수를 입력하세요.');
+    }else{
+        alert('갯수가 입력되었습니다.');
+    }
+}
+
 if (name){
     name.addEventListener("click", ClickName);
+}
+
+if (price){
+    price.addEventListener("click", ClickPrice);
+}
+
+if (number){
+    number.addEventListener("click", ClickNumber);
 }
