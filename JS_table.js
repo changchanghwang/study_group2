808894b17eb1f542956e72452a08ee936fcc004a
CH_Hwang <hch950627@naver.com>
2021-07-31 12:56:21 +0900

    server complete

--- JS_table.js
+++ JS_table.js
@@ -1,3 +1,8 @@
+const editBtn = document.querySelector(".modalSt #stock_btn")
+const deleteBtn = document.querySelector(".modalSt #D_stock_btn")
+const namesize = document.querySelector(".modalSt #inputname")
+const pricesize = document.querySelector(".modalSt #inputprice")
+const numbersize = document.querySelector(".modalSt #inputnumber")
 
 function openclose(){
     let status = $('#stocktable').css('display');
@@ -18,18 +23,16 @@ $(document).ready(function(){
 
 function rate(){
     $.ajax({
-            type: "GET",
-            url: "http://spartacodingclub.shop/sparta_api/rate",
-            data: {},
-            success: function (response){
-                if (response["result"] == "success") {
-                    let rates = response['rate'];
-                    let temp_html = ""
-                    temp_html =`<span class="rates">${rates+'원'}</span>`
-                    $('#rate').append(temp_html)
-                }
-            }
-        })
+        type: "GET",
+        url: "http://spartacodingclub.shop/sparta_api/rate",
+        data: {},
+        success: function (response){
+            let rates = response['rate'];
+            let temp_html = ""
+            temp_html =`<span class="rates">${rates+'원'}</span>`
+            $('#rate').append(temp_html)
+        }
+    })
 }
 
 function myStock() {
@@ -38,21 +41,125 @@ function myStock() {
         url: "/Stock",
         data: {},
         success: function (response) {
-            let stocks = response['view_stock']
-            for (let i = 0; i<stocks.length; i++){
-                let ticker = stocks[i]['ticker']
-                let price = stocks[i]['price']
-                let quantity = stocks[i]['quantity']
-                let total = price * quantity
-                let temp_html = `
-                                <tbody>
-                                    <th>${ticker}</th>
-                                    <th>${price}</th>
-                                    <th>${quantity}</th>
-                                    <th>${total}</th>
-                                </tbody>`
-                $('#stocktable').append(temp_html);
+            if (response["result"] == "success") {
+                let stocks = response['view_stock']
+                for (let i = 0; i<stocks.length; i++){
+                    let ticker = stocks[i]['ticker']
+                    let price = stocks[i]['price']
+                    let quantity = stocks[i]['quantity']
+                    let total = (price * quantity).toFixed(2)
+                    let temp_html = `
+                                    <tbody>
+                                        <th>${ticker}</th>
+                                        <th>${price}</th>
+                                        <th>${quantity}</th>
+                                        <th>${total}</th>
+                                    </tbody>`
+                    $('#stocktable').append(temp_html);
+                }
             }
         }
     })
 }
+
+function editStock(){
+    let ticker = $('#inputname').val();
+    let price = $('#inputprice').val();
+    let quantity = $('#inputnumber').val();
+    
+    if(namesize.value === ""){
+        alert('종목을 입력하세요.');
+    }
+    if(pricesize.value ===""){
+        alert('평단가를 입력하세요.')
+    }
+    if(numbersize.value ===""){
+        alert('갯수를 입력하세요.')
+    } else{
+        $.ajax({
+            type: 'POST',
+            url: '/Stock/Edit',
+            data: {ticker_give:ticker, price_give:price, quantity_give:quantity},
+            success: function (response) {
+                alert(response['msg']);
+                window.location.reload()
+            }
+        });
+    }
+}
+function deleteStock(){
+    let ticker = $('#delete_stock').val();
+    if(namesize.value === "삭제할 종목을 골라주세요"){
+        alert('종목을 입력하세요.');
+    }else{
+        $.ajax({
+            type: 'POST',
+            url: '/Stock/Delete',
+            data: {ticker_give:ticker},
+            success: function (response) {
+                alert(response['msg']);
+                window.location.reload()
+            }
+        });
+    }
+}
+function stock_list(){
+    $.ajax({
+        type: "GET",
+        url: "/lists",
+        data: {},
+        success: function (response) {
+            let stocks = response['stock_list']
+            for (let i = 0; i<stocks.length; i++){
+                let ticker = stocks[i]['ticker']
+                let temp_html = `<option value="${ticker}">${ticker}</option>`
+                $('#inputname').append(temp_html);
+            }     
+        }
+    })
+}
+function Delete_list(){
+    $.ajax({
+        type: "GET",
+        url: "/D_lists",
+        data: {},
+        success: function (response) {
+            let stocks = response['Delete_list']
+            for (let i = 0; i<stocks.length; i++){
+                let ticker = stocks[i]['ticker']
+                let temp_html = `<option value="${ticker}">${ticker}</option>`
+                $('#delete_stock').append(temp_html);
+            }     
+        }
+    })
+}
+
+
+function Ed_onClick() {
+    document.querySelector('#Ed_modal_wrap').style.display ='block';
+    document.querySelector('#Ed_black_bg').style.display ='block';
+    return stock_list();
+}   
+function Ed_offClick() {
+    document.querySelector('#Ed_modal_wrap').style.display ='none';
+    document.querySelector('#Ed_black_bg').style.display ='none';
+    window.location.reload();
+}
+
+function D_onClick() {
+    document.querySelector('#D_modal_wrap').style.display ='block';
+    document.querySelector('#D_black_bg').style.display ='block';
+    return Delete_list();
+}   
+function D_offClick() {
+    document.querySelector('#D_modal_wrap').style.display ='none';
+    document.querySelector('#D_black_bg').style.display ='none';
+    window.location.reload();
+}
+
+document.querySelector('#Ed_modal_btn').addEventListener('click', Ed_onClick);
+document.querySelector('#Ed_modal_close').addEventListener('click', Ed_offClick);
+document.querySelector('#D_modal_btn').addEventListener('click', D_onClick);
+document.querySelector('#D_modal_close').addEventListener('click', D_offClick);
+editBtn.addEventListener("click", editStock);
+deleteBtn.addEventListener("click", deleteStock);
--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client.dbindex
 
 
@@ -13,28 +14,64 @@ db = client.dbindex
 def index():
     return render_template('index.html')
 
+
 @app.route('/my_stock')
 def table():
     return render_template('my_stock.html')
 
-# 주문하기(POST) API
+
+# 종목 저장 (POST) API
 @app.route('/Stock', methods=['POST'])
 def save_stock():
     ticker_receive = request.form['ticker_give']
     price_receive = request.form['price_give']
     quantity_receive = request.form['quantity_give']
 
-    doc ={'ticker':ticker_receive,'price':price_receive,'quantity':quantity_receive}
+    doc ={'ticker':ticker_receive,'price':price_receive,'quantity':quantity_receive, 'total':float(price_receive)*int(quantity_receive)}
     db.stock.insert_one(doc)
 
     return jsonify({'result': 'success', 'msg': '저장완료!'})
 
 
-# 주문 목록보기(Read) API
+# 목록 보기 및 편집(Read,edit) API
 @app.route('/Stock', methods=['GET'])
 def view_stock():
-    my_stock= list(db.stock.find({}, {'_id': False}))
+    my_stock = list(db.stock.find({}, {'_id': False}).sort('total', -1))
     return jsonify({'result': 'success','view_stock': my_stock})
+
+
+@app.route('/lists', methods=['GET'])
+def stock_list():
+    stocks = list(db.stock.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'stock_list': stocks})
+
+
+@app.route('/Stock/Edit', methods=['POST'])
+def edit_stock():
+    ticker_receive = request.form['ticker_give']
+    quantity_receive = request.form['quantity_give']
+    price_receive = request.form['price_give']
+    target_stock = db.stock.find_one({'ticker': ticker_receive})
+    current_quantity = target_stock['quantity']
+    current_price = float(target_stock['price']) * int(current_quantity)
+    new_quantity = int(current_quantity) + int(quantity_receive)
+    new_price = (float(current_price) + (float(price_receive) * int(quantity_receive))) / int(new_quantity)
+
+    db.stock.update_one({'ticker': ticker_receive}, {'$set': {'quantity': new_quantity, 'price': new_price}})
+    return jsonify({'msg': '편집되었습니다!'})
+
+
+@app.route('/Stock/Delete', methods=['POST'])
+def delete_star():
+    ticker_receive = request.form['ticker_give']
+    db.stock.delete_one({'ticker': ticker_receive})
+    return jsonify({'msg': '삭제되었습니다.'})
+
+
+@app.route('/D_lists', methods=['GET'])
+def delete_list():
+    delete = list(db.stock.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'Delete_list': delete})
 
 
 if __name__ == '__main__':
